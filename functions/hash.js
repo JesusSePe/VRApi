c79528220b4f1f7050ef78a81f41b1baee050e78
JesusSePe <umbreon20019@gmail.com>
2022-03-11 17:49:52 +0100

    Login and logout endpoints created #3

---
+++ functions/hash.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto');
+
+module.exports = {
+    // Returns a sha-256 hash from a given string.
+    sha265: function(string) {
+        return crypto.createHash('sha256').update(string, 'utf-8').digest('hex');
+    },
+    // Create a random 6 digit string from 0 to 999999.
+    salt: function(){
+        return crypto.randomInt(0, 1000000).toString().padStart(6, "0");
+    }
+}
--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const expiration = process.env.TOKEN_EXPIRATION_TIME;
 // Mongoose models
 var user = require('./models/user').user;
 
+// Functions
+const hash = require('./functions/hash.js'); // Hashing functions
+const moment = require('moment');
+const { json } = require('express/lib/response');
+
+
 // Mongo connection
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:' ));
 
-
+app.use(express.urlencoded());
 
 // Root directory
 app.get('/', (req, res) => {
@@ -26,6 +32,101 @@ app.get('/', (req, res) => {
     const users = await user.find({});
     res.json(users);
 });*/
+
+// login directory
+app.get('/api/login', async (req, res) => {
+    // retrieve user and password
+    var usuari = req.body.usr;
+    var contrasenya = req.body.pass;
+
+    // Results
+    let status = "ERROR";
+    let message = "";
+    let session_token = "";
+
+    // Check username
+    if (typeof(usuari) != "undefined") {
+        let usr = await user.findOne({ first_name: usuari });
+        
+        if (usr != null) {
+            // Check password
+            if (hash.sha265(contrasenya) == usr.password) {
+                let exp_date = moment().add(expiration, 'seconds');
+                status = "OK";
+                message = "Login success";
+                session_token = hash.sha265(usr.email + usr.password + hash.salt());
+                user.findOneAndUpdate( { email: usr.email }, { "$set": { "session_token": session_token, "session_token_exp_date": exp_date }} ).exec();
+                res.json({status: status, message: message, session_token: session_token});
+            }
+            // Password does not match
+            else {
+                message = "Wrong credentials"
+                res.json({status: status, message: message});
+            }
+        }
+        // Username not found
+        else {
+            console.log(usr.password);
+            message = "Authentication error"
+            res.json({status: status, message: message});
+        }
+    }
+    // Username not sent
+    else {
+        message = "username is required";
+        res.json({status: status, message: message});
+    }
+});
+
+// Logout
+app.get('/api/logout', async (req, res) => {
+    // retrieve user token
+    var tkn = req.body.session_token;
+
+    // Default messages
+    let status = "ERROR";
+    let message = "";
+
+    if (typeof(tkn) != "undefined") {
+
+        let usr = await user.findOne({ session_token: tkn });
+
+        if (usr != null) {
+
+            console.log(moment(usr.session_token_exp_date).utc().valueOf());
+            console.log(moment().utc().valueOf());
+
+            if (moment(usr.session_token_exp_date).utc().valueOf() > moment().utc().valueOf()) {
+
+                status = "OK";
+                message = "Session successfully closed.";
+
+                user.findOneAndUpdate( { session_token: tkn }, { "$set": { "session_token": null, "session_token_exp_date": null }} ).exec();
+
+                res.json({status: status, message: message});
+
+            }
+            // Token is expired
+            else {
+                message = "Expired token"
+                res.json({ status: status, message: message });
+            }
+
+        }
+        // user token not found
+        else {
+            message = "invalid token"
+            res.json({ status: status, message: message });
+        }
+
+    }
+    // Token has not been sent
+    else {
+        message = "session_token is required."
+        res.json({ status: status, message: message });
+    }
+
+});
 
 // Crear un servidor web con express el el puerto asignado en la varible port.
 app.listen(port, () => {
