7f64319af9de487870261b8421e577afce8b4a59
JesusSePe <umbreon20019@gmail.com>
2022-03-30 17:54:52 +0200

    hotfix api

--- functions/endpoints.js
+++ functions/endpoints.js
@@ -320,7 +320,11 @@ module.exports = {
         }
 
         // Check if pin exists
-        var pinData = await pin.findOne({ pin_number: parseInt(PIN) });
+        try {
+            var pinData = await pin.findOne({ pin_number: parseInt(PIN) });
+        } catch {
+            var pinData = null;
+        }
 
         if (pinData == null) {
 
